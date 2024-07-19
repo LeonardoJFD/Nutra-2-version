0cf888241bc5818caab29886b6aa3cc6153183a7
LeonardoJFD <leonardojosefonsecadono@gmail.com>
2024-07-18 20:25:25 -0400

    Mi segunda version de Nutra, hecha para ser modificada

---
+++ views/paginas/nutra.php
@@ -0,0 +1,20 @@
+<main class="nutra">
+    <h2 class="nutra__heading"><?php echo $titulo; ?></h2>
+    <p class="nutra__descripcion">Conoce la conferencia más importante de Latinoamérica</p>
+
+    <div class="nutra__grid">
+        <div class="nutra__imagen">
+            <picture>
+                <source srcset="build/img/sobre_nutra.avif" type="image/avif">
+                <source srcset="build/img/sobre_nutra.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_nutra.jpg" alt="Imagen Nutra">
+            </picture>
+        </div>
+
+        <div  class="nutra__contenido">
+            <p  class="nutra__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+            
+            <p  class="nutra__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+        </div>
+    </div>
+</main>
